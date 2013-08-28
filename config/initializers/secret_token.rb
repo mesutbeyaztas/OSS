2666955bf032cae1683770e901bed1560e353de0
OCC <occ@occ-R560.(none)>
2013-08-28 09:58:36 +0300

    Inıtial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OSS::Application.config.secret_key_base = 'b49f7c620f3762d44eaedb1a8779792fde03d47d13735ccad8d8b9fa4247d338b5b8e2a3e857debc836a66692e2d84df0607d8761ae53450ff6a05e949081297'
